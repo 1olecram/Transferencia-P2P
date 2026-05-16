f75ff88c5a72dbc576dc4544fed792d2447e323e
1olecram <marcelochave059@gmail.com>
2026-05-16 12:59:52 -0300

    feat: separa chunks em arquivos diferentes

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-def read_file(file_path, size):
+import os
+
+def split_file(file_path, size, output_dir='parts'):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+        
     with open(file_path, 'rb') as f:
+        chunk_num = 0
         while True:
             chunk = f.read(size)
             if not chunk:
                 break
             
-            print(chunk)
+            chunk_path = os.path.join(output_dir, f'chunk_{chunk_num}')
+            with open(chunk_path, 'wb') as chunk_file:
+                chunk_file.write(chunk)
+            print(f"Salvo: {chunk_path}")
+            
+            chunk_num += 1
 
 def main():
-    size = 1024 
-    read_file('base_files/file_A.bin', size)
+    size = 1024 # 1kb
+    split_file('base_files/file_A.bin', size)
 
 if __name__ == "__main__":
     main()
-
